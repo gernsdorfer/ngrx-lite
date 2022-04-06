0894c224e0770478cbabba244dd10636c006e924
gernsdorfer <larswiedemann@gmx.net>
2022-04-06 09:49:34 +0200

    output warning for broken doku links

--- apps/doc-app/docusaurus.config.js
+++ apps/doc-app/docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'A Small And fast Redux Store',
   url: 'https://www.google.de',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'gernsdorfer', // Usually your GitHub org/user name.
