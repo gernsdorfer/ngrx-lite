41b4bfea781a2ebd9676f7ba1d514d46aaa6406e
gernsdorfer <larswiedemann@gmx.net>
2022-05-19 07:22:27 +0200

    update examples

---
+++ apps/sample-app/src/app/component-store/store-without-loging/store-without-log.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy } from '@angular/core';
+import { StoreFactory } from '@gernsdorfer/ngrx-lite';
+
+export interface MyState {
+  counter: number;
+}
+
+@Component({
+  selector: 'my-app-store-without-loging',
+  templateUrl: 'store-without-log.html',
+})
+export class StoreWithoutLogComponent implements OnDestroy {
+  private store = this.storeFactory.createComponentStore<MyState>({
+    storeName: 'STORE_WITHOUT_LOG',
+    defaultState: { counter: 0 },
+    skipLog: true
+  });
+
+  public counterState$ = this.store.state$;
+
+  constructor(private storeFactory: StoreFactory) {}
+
+  increment(counter: number) {
+    // patch your state
+    this.store.patchState({ counter });
+  }
+
+  ngOnDestroy() {
+    this.store.ngOnDestroy();
+  }
+}
