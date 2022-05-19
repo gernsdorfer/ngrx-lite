9b1a2027674ed3ea220fe30c95e22d81dd29fcb6
gernsdorfer <larswiedemann@gmx.net>
2022-05-19 07:11:34 +0200

    fix lint error any

--- libs/store/src/services/stores/component-store.service.ts
+++ libs/store/src/services/stores/component-store.service.ts
@@ -46,6 +46,7 @@ export class ComponentStore<
     }
     const newState =
       typeof stateOrUpdaterFn === 'function'
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         ? (stateOrUpdaterFn as unknown as any)(this.get())
         : stateOrUpdaterFn;
     super.setState(newState);
