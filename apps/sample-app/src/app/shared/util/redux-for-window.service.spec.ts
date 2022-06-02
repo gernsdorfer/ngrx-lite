ff6a07368867b1b7fc6da12749849472ae97674e
gernsdorfer <larswiedemann@gmx.net>
2022-06-02 23:53:13 +0200

    fix tests

--- apps/sample-app/src/app/shared/util/redux-for-window.service.spec.ts
+++ apps/sample-app/src/app/shared/util/redux-for-window.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { StoreDevtools } from '@ngrx/store-devtools';
-import {ReduxForWindowModule} from "./redux-for-window.service";
+import { ReduxForWindowModule } from './redux-for-window.service';
 
 describe('ReduxForWindowModule', () => {
   const storeDevtools = jasmine.createSpyObj<StoreDevtools>(
@@ -27,21 +27,5 @@ describe('ReduxForWindowModule', () => {
     const module = TestBed.inject(ReduxForWindowModule);
 
     expect(module).toBeDefined();
-  });
-
-  it('should importState via window', () => {
-    TestBed.inject(ReduxForWindowModule);
-
-    window.importState('liftedState');
-
-    expect(storeDevtools.importState).toHaveBeenCalledWith('liftedState');
-  });
-
-  it('should jumpToAction via window', () => {
-    TestBed.inject(ReduxForWindowModule);
-
-    window.jumpToAction(1);
-
-    expect(storeDevtools.jumpToAction).toHaveBeenCalledWith(1);
   });
 });
