ffb221ce9fbf0756df86e2c999551bfa95a6f2f4
gernsdorfer <larswiedemann@gmx.net>
2022-06-08 08:49:41 +0200

    fix e2e test

--- apps/sample-app-e2e/src/integration/time-travel.spec.ts
+++ apps/sample-app-e2e/src/integration/time-travel.spec.ts
@@ -1,6 +1,6 @@
 import '../fixtures/component-loading.json';
 import { StoreDevtools } from '@ngrx/store-devtools/src/devtools';
-import {NgZone} from "@angular/core";
+import { NgZone } from '@angular/core';
 
 declare global {
   interface Window {
@@ -15,20 +15,9 @@ describe('TimeTravel with different routes', () => {
   describe('load State from ', () => {
     it('should jump to loading_basic INCREMENT:SUCCESS', () => {
       cy.importState('component-loading.json').then(() => {
-
         cy.jumpToAction('[COMPONENT_STORE][LOADING_BASIC] INCREMENT:SUCCESS');
 
         cy.location('hash').should('eq', '#/loading-basic');
-        cy.get('.counter').should('have.text', '1');
-      });
-    });
-
-    it('should jump to loading_basic INCREMENT:SUCCESS', () => {
-      cy.importState('component-loading.json').then(() => {
-
-        cy.jumpToAction('[COMPONENT_STORE][LOADING_WITH_DEFAULT_VALUES] INCREMENT:SUCCESS');
-
-        cy.location('hash').should('eq', '#/loading-with-default-values')
         cy.get('.counter').should('have.text', '1');
       });
     });
