1c3e8dbedafccfc7baf31d87f562db14e4d4559d
gernsdorfer <lwiedemann@MBP-von-Lars.fritz.box>
2023-05-11 07:31:40 +0200

    update to nx 16

---
+++ apps/sample-app/src/app/app.config.ts
@@ -0,0 +1,41 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import {
+  LocalStoragePlugin,
+  RouterStoreModule,
+  SessionStoragePlugin,
+  localStoragePlugin,
+  sessionStoragePlugin,
+} from '@gernsdorfer/ngrx-lite';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { routes } from './app-routing.module';
+import { ReduxForWindowModule } from './shared/util/redux-for-window.service';
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(BrowserModule),
+    importProvidersFrom(BrowserAnimationsModule),
+    importProvidersFrom(StoreModule.forRoot({})),
+    importProvidersFrom(EffectsModule.forRoot([])),
+    importProvidersFrom(
+      StoreDevtoolsModule.instrument({
+        name: 'ngrx-lite-demo',
+        maxAge: 25,
+        logOnly: false,
+        monitor: (state, action) => action,
+      })
+    ),
+    importProvidersFrom(RouterStoreModule),
+    importProvidersFrom(ReduxForWindowModule),
+    importProvidersFrom(
+      RouterModule.forRoot(routes, {
+        useHash: true,
+      })
+    ),
+    { provide: SessionStoragePlugin, useValue: sessionStoragePlugin },
+    { provide: LocalStoragePlugin, useValue: localStoragePlugin },
+  ],
+};
--- apps/sample-app/src/main.ts
+++ apps/sample-app/src/main.ts
@@ -1,48 +1,13 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import {
-  localStoragePlugin,
-  LocalStoragePlugin,
-  RouterStoreModule,
-  sessionStoragePlugin,
-  SessionStoragePlugin,
-} from '@gernsdorfer/ngrx-lite';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { routes } from './app/app-routing.module';
+import { appConfig } from './app/app.config';
+
 import { AppComponent } from './app/app.component';
-import { ReduxForWindowModule } from './app/shared/util/redux-for-window.service';
+
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(BrowserModule),
-    importProvidersFrom(BrowserAnimationsModule),
-    importProvidersFrom(StoreModule.forRoot({})),
-    importProvidersFrom(EffectsModule.forRoot([])),
-    importProvidersFrom(
-      StoreDevtoolsModule.instrument({
-        name: 'ngrx-lite-demo',
-        maxAge: 25,
-        logOnly: false,
-        monitor: (state, action) => action,
-      })
-    ),
-    importProvidersFrom(RouterStoreModule),
-    importProvidersFrom(ReduxForWindowModule),
-    importProvidersFrom(
-      RouterModule.forRoot(routes, {
-        useHash: true,
-      })
-    ),
-    { provide: SessionStoragePlugin, useValue: sessionStoragePlugin },
-    { provide: LocalStoragePlugin, useValue: localStoragePlugin },
-  ],
-});
+bootstrapApplication(AppComponent, appConfig);
