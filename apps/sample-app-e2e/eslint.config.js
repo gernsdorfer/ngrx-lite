2c2806f277794c922b282604923196cedde2b914
gernsdorfer <larswiedemann@gmx.net>
2026-01-07 13:32:32 +0100

    update libs

--- apps/sample-app-e2e/eslint.config.js
+++ apps/sample-app-e2e/eslint.config.js
@@ -1,15 +1,16 @@
 const { FlatCompat } = require('@eslint/eslintrc');
 const js = require('@eslint/js');
 const baseConfig = require('../../eslint.config.js');
+const cypress = require('eslint-plugin-cypress/flat');
 
-const compat = new FlatCompat({
+new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
 });
 
 module.exports = [
   ...baseConfig,
-  ...compat.extends('plugin:cypress/recommended'),
+  cypress.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     // Override or add rules here
